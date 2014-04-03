40573cf706e988ed8a4fb8ae1b606068bb2e2b2c
pabitraadhikari@gmail.com <pabitraadhikari@gmail.com>
2014-04-03 12:07:29 -0700

    REST delete to delete a book

--- server.js
+++ server.js
@@ -116,10 +116,20 @@ app.put( '/api/books/:id', function( request, response ) {
 	});
 });
 
-
-
-
-
+//Delete a book
+app.delete( '/api/books/:id', function( request, response ) {
+	console.log( 'Deleting book with id: ' + request.params.id );
+	return BookModel.findById( request.params.id, function( err, book ) {
+		return book.remove( function( err ) {
+			if( !err ) {
+				console.log( 'Book removed' );
+				return response.send( '' );
+			} else {
+				console.log( err );
+			}
+		});
+	});
+});
 
 //start server
 var port = 4714;
