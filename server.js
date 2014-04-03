d7db1bb91a2b03db4756adcb529791d787be6d9d
pabitraadhikari@gmail.com <pabitraadhikari@gmail.com>
2014-04-03 09:34:26 -0700

    create and configure web server

---
+++ server.js
@@ -0,0 +1,32 @@
+// module dependencies
+
+var application_root = _dirname,
+	express = require('express'),
+	path = require('path'),
+	mongoose = require('mongoose');
+
+
+// create server
+var app = express();
+
+//configure server
+app.configure(function(){
+
+	// parse request body and populates request.body
+	app.use(express.bodyParser());
+	
+	// check request.bdy for http methos override
+	app.use(express.methodOverride());
+
+	//perform route lookup based on URL and HTTP method
+	app.use(app.router);
+
+	//where to serve static content
+	app.use(express.static(path.join(application_root,'site')));
+
+	//show all errors in development
+	app.use(express.errorHandler({dumpExceptions:true, showStack:true}));
+
+
+
+});
